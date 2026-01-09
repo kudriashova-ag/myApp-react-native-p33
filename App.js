1d973d0451495a9d3e6dbfff9cf94556153fd206
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2026-01-09 20:48:00 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const pressHandler = () => {
+    alert('You pressed')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.heading} onPress={pressHandler}>My App</Text>
+      <Text>My first <Text style={styles.bold}>React Native</Text> App</Text>
+
+      
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ backgroundColor: '#ddd', flex: 2 }}>
+          <Text>Some text</Text>
+        </View>
+        <View style={[styles.container, {backgroundColor: '#555'}]}>
+          <Text>Some text</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  heading: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  bold: {
+    fontWeight: 'bold'
+  }
 });
